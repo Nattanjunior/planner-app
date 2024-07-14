4887154cec5808086e174bb5843c56f284c0c54d
nattanjunior <fnatanieljunior@gmail.com>
2024-07-14 02:56:03 -0300

    Modal de adicionar datas de incio e fim da viagem, foi adicionado, Modal de adicionar email de convite a viagem, tambem foi adicionado, validações de email e exibição dos emails.

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,10 +1,30 @@
 import { useState } from 'react';
-import { View, Text , Image} from 'react-native';
-import {Input} from '@/components/input'
-import { Button } from '@/components/button';
+import { View, Text , Image, Keyboard, Alert} from 'react-native';
 
-import {MapPin, Calendar as IconCalendar, Settings2, UserRoundPlus, ArrowRight} from 'lucide-react-native'
+import dayjs from 'dayjs';
+import { DateData } from 'react-native-calendars';
+
+import {
+  MapPin, 
+  Calendar as IconCalendar, 
+  Settings2, 
+  UserRoundPlus, 
+  ArrowRight,
+  AtSign
+} from 'lucide-react-native'
+
+import {Input} from '@/components/input'
+import { calendarUtils, DatesSelected} from '@/utils/calendarUtils' 
+
+import { Button } from '@/components/button';
 import { colors } from '@/styles/colors';
+
+import { Modal } from '@/components/modal';
+
+import { Calendar } from '@/components/calendar';
+import { GuestEmail } from '@/components/email';
+import { validateInput } from '@/utils/validateInput';
+
 
 
 enum StepForm{
@@ -12,13 +32,72 @@ enum StepForm{
   ADD_EMAIL = 2,
 }
 
+enum MODAL{
+  NONE = 0,
+  CALENDAR = 1,
+  GUESTS = 2
+}
+
 export default function Index() {
+  // DATES
   const [stepForm, setStepForm] = useState(StepForm.TRIP_DETAILS)
+  const [selectedDates, setSelectdeDates] = useState({} as DatesSelected)
+  const [destination, setDestination] = useState("")
+  const [emailToInvite, setEmailToInvite] = useState("")
+  const [emailsToInvite, setEmailsToInvite] = useState<string[]>([])
+
+  // MODAL
+  const [showModal, setShowModal] = useState(MODAL.NONE)
+
 
   function handleNextStepForm(){
+    if(destination.trim().length === 0 || !selectedDates.startsAt || !selectedDates.endsAt){
+      return Alert.alert(
+        "Detalhes da viagem",
+        "Preencha todas as informações da viagem para seguir."
+      )
+    }
+
+
+    if(destination.length < 4){
+      return Alert.alert(
+        "Detalhes da viagem",
+        "O destino deve ter pelo menos 4 caracteres."
+      )
+    }
+
+
     if(stepForm === StepForm.TRIP_DETAILS){
       return setStepForm(StepForm.ADD_EMAIL)
     }
+  }
+
+  function handleSelectedStepForm(selectedDay: DateData){
+    const dates = calendarUtils.orderStartsAtAndEndsAt({
+      startsAt: selectedDates.startsAt,
+      endsAt: selectedDates.endsAt,
+      selectedDay
+    })
+
+    setSelectdeDates(dates)
+  } 
+
+  function handleRemoveEmail(emailToRemove: string){
+    setEmailsToInvite((prevState)=> prevState.filter((email) => email !== emailToRemove)
+    )
+  }
+
+  function handleAddEmail(){
+    if(!validateInput.email(emailToInvite)){
+      return Alert.alert('Convidado', "E-mail inválido!" )
+    }
+
+    const emailAlreadyExists = emailsToInvite.find((email)=> email === emailToInvite)
+    if(emailAlreadyExists){
+      return Alert.alert('Convidado', "E-mail já foii adicionado!" ) 
+    }
+    setEmailsToInvite((prevState)=> [...prevState, emailToInvite])
+    setEmailToInvite("")
   }
 
 
@@ -45,7 +124,10 @@ export default function Index() {
         <MapPin color={colors.zinc[400]} size={20}/>
         <Input.Field 
           placeholder='Para onde?' 
-          editable={stepForm === StepForm.TRIP_DETAILS}/>  
+          editable={stepForm === StepForm.TRIP_DETAILS}
+          onChangeText={setDestination}
+          value={destination}
+          />
       </Input>
 
 
@@ -54,7 +136,12 @@ export default function Index() {
         <Input.Field 
           placeholder='Quando?'
           editable={stepForm === StepForm.TRIP_DETAILS}
-          />  
+          onFocus={()=> Keyboard.dismiss()}
+          showSoftInputOnFocus={false}
+          onPressIn={()=> stepForm === StepForm.TRIP_DETAILS && setShowModal(MODAL.CALENDAR)}
+          value={selectedDates.formatDatesInText}
+        
+        />  
       </Input>
 
         {stepForm === StepForm.ADD_EMAIL && (
@@ -92,6 +179,66 @@ export default function Index() {
       Ao planejar sua viagem pela plann.er Você automaticamente concorda com nossos{" "}
       <Text className='text-zinc-300 underline'>termos de uso e políticas de privacidade.</Text>
     </Text>
+
+    <Modal
+      title='Selecionar datas'
+      subtitle='Selecione a data de ida e volta da viagem'
+      onClose={()=> setShowModal(MODAL.NONE)}
+      visible={showModal === MODAL.CALENDAR}
+    >
+      <View className='gap-2 mt-4'>
+        <Calendar 
+         minDate={dayjs().toISOString()}
+         onDayPress={handleSelectedStepForm}
+         markedDates={selectedDates.dates}
+        
+        />
+
+        <Button onPress={()=> setShowModal(MODAL.NONE)}>
+          <Button.Title>Confirmar</Button.Title>
+        </Button>
+      </View>
+    </Modal>
+
+    <Modal
+      title='Selecionar convidados'
+      subtitle='Os convidados irão receber e-mails para confirmar a participação na viagem.'
+    >
+
+      <View className='my-2 flex-col gap-2 border-b border-zinc-800 py-5 items-start'>
+        { emailsToInvite.length > 0 ? (
+          emailsToInvite.map((email)=>(
+            <GuestEmail
+            key={email}
+            email={email}
+            onRemove={()=> handleRemoveEmail(email) }
+          />
+          ))
+        ) : (
+          <Text className='text-zinc-600 text-base font-regular'>Nenhum e-mail adicionado.</Text>
+        )}
+       
+      </View>        
+
+      <View className='gap-4 mt-4'>
+        <Input variants='secondary'>
+          <AtSign color={colors.zinc[400]} size={20}/>
+          <Input.Field 
+            placeholder='Digite o e-mail do convidado'
+            keyboardType='email-address'
+            onChangeText={(text) => setEmailToInvite(text.toLocaleLowerCase())}
+            value={emailToInvite}
+            returnKeyType='send'
+            onSubmitEditing={handleAddEmail}
+          />
+        </Input>
+
+        <Button onPress={handleAddEmail}>
+          <Button.Title>Convidar</Button.Title>
+        </Button>
+
+      </View>
+    </Modal>
 
    </View>
   );
